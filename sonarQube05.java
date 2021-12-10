47c7c1f64e80c18f8417d69833838a1c27e71d06
Kadiatou Yattassaye <93450637+Kadiatou-yatt@users.noreply.github.com>
2021-12-10 15:26:13 +0100

    Update sonarQube05.java

--- sonarQube05.java
+++ sonarQube05.java
@@ -1,6 +1,6 @@
 public class sonarQube05 {
     public int doDivide(int num, boolean isTrue){
-        int divisor = 0;
+        int divisor = 1;
         if (isTrue) divisor = 20;
         return num / divisor;
     }
